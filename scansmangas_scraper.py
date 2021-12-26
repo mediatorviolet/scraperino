4f76f2923858bd31bb70f71fc4366a17415f3794
Antoine SCHILT <antoineschilt@gmail.com>
2021-12-26 19:52:04 +0400

    add: add docstring

--- scansmangas_scraper.py
+++ scansmangas_scraper.py
@@ -8,11 +8,18 @@ from bs4 import BeautifulSoup
 # Todo: Après le dl, faire function pour grouper les jpg. Si oui, on peut choisir le format (d'abord .pdf)
 # Todo: lier le script avec google drive pour envoyer le fichier sur le drive et pouvoir le récup sur phone
 # Todo: sur le long terme (ou pas), modulariser le programme pour pouvoir l'utiliser sur d'autres sites
-# Todo: écrire des docstring pour les functions
 # Todo: print un message au start
 
 
 def get_page(url, i=''):
+    """Get html page from url
+
+    :param url: str
+    :param i: str
+
+    :return soup: bytes
+    """
+
     req = requests.get(url + i)
     soup = BeautifulSoup(req.text, 'html.parser')
 
@@ -20,6 +27,13 @@ def get_page(url, i=''):
 
 
 def get_page_nb(first_page):
+    """Get the total number of page from the first page
+
+    :param first_page:
+
+    :return: page_nb: str
+    """
+
     nav = first_page.find('div', class_='nav_apb')
     page_list = nav.find_all('a')
     page_nb = page_list[len(page_list) - 2].text
@@ -28,6 +42,12 @@ def get_page_nb(first_page):
 
 
 def get_scans_from_all_pages(page_nb, url):
+    """Create a list of all scans from all the pages
+
+    :param page_nb: str
+    :param url: str
+    :return: scan_list: list
+    """
     scans_list = []
     for i in range(1, int(page_nb) + 1):
         page = get_page(url + str(i))
@@ -40,12 +60,22 @@ def get_scans_from_all_pages(page_nb, url):
 
 
 def get_dir_name(url):
+    """Get directory name from html page
+
+    :param url: str
+    :return: name: str
+    """
     name = re.search(r"(?<=/scan-)(.*)(?=/)", url).group(1)
 
     return name
 
 
 def set_working_dir(name):
+    """Create new directory and cd into it
+
+    :param name: str
+    :return: void
+    """
     dir_name = os.path.join(os.getcwd(), 'loot/' + name)
 
     if os.path.exists(dir_name):
@@ -67,6 +97,11 @@ def set_working_dir(name):
 
 
 def scan_down(scans):
+    """Download all scans from list
+
+    :param scans: list
+    :return: void
+    """
     for scan in scans:
         filename = re.search(r"(?<=Scan )(.*)", scan['alt']).group(1).replace(' ', '-') + '.jpg'
         with open(filename, 'wb') as f_out:
